fbac1a2101b7f7624d15450aa13c2f68ef566b85
Austin Bennett <bboywake@gmail.com>
2019-09-16 15:12:57 -0600

    Added vuetify loader and tweaking some layout

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,57 @@
 <template>
-  <div id="app">
-    <section class="row">
-      <div class="small-6 columns">
-          <h1 class="text-center">YOU</h1>
-          <div class="healthbar">
-              <div
-                class="healthbar text-center"
-                style="background-color: green; margin: 0; color: white;"
-                :style="{width: playerHealth + '%'}">
-                {{ playerHealth }}
-              </div>
-          </div>
-      </div>
-      <div class="small-6 columns">
-          <h1 class="text-center">MONSTER</h1>
-          <div class="healthbar">
-              <div
-                      class="healthbar text-center"
-                      style="background-color: green; margin: 0; color: white;"
-                      :style="{width: monsterHealth + '%'}">
-                  {{ monsterHealth }}
-              </div>
-          </div>
-      </div>
-    </section>
-    <section class="row controls" v-if="!gameIsRunning">
-      <div class="small-12 columns">
-          <button id="start-game" @click="startGame">START NEW GAME</button>
-      </div>
-    </section>
-    <section class="row controls" v-else>
-      <div class="small-12 columns">
-        <button id="attack" @click="attack">ATTACK</button>
-        <button id="special-attack" @click="specialAttack">SPECIAL ATTACK</button>
-        <button id="heal" @click="heal">HEAL</button>
-        <button id="give-up" @click="giveUp">GIVE UP</button>
-      </div>
-    </section>
-    <section class="row log" v-if="turns.length > 0">
-      <div class="small-12 columns">
-        <ul>
-          <li v-for="turn in turns"
-            :class="{'player-turn': turn.isPlayer, 'monster-turn': !turn.isPlayer}" v-bind:key="turn.id">
-            {{ turn.text }}
-          </li>
-        </ul>
-      </div>
-    </section>
-  </div>
+	<div id="app">
+		<v-container fluid>
+			<v-row>
+				<v-col cols="6">
+					<v-card class="pa-2">
+						<h1 class="text-center">YOU</h1>
+						<div class="healthbar">
+							<div
+								class="healthbar text-center"
+								style="background-color: green; margin: 0; color: white;"
+								:style="{width: playerHealth + '%'}">
+								{{ playerHealth }}
+							</div>
+						</div>
+					</v-card>
+				</v-col>
+				<v-col cols="6">
+					<h1 class="text-center">MONSTER</h1>
+					<div class="healthbar">
+						<div
+								class="healthbar text-center"
+								style="background-color: green; margin: 0; color: white;"
+								:style="{width: monsterHealth + '%'}">
+							{{ monsterHealth }}
+						</div>
+					</div>
+				</v-col>
+			</v-row>
+			<section class="row controls" v-if="!gameIsRunning">
+				<div class="small-12 columns">
+					<v-btn id="start-game" @click="startGame">START NEW GAME</v-btn>
+				</div>
+			</section>
+			<section class="row controls" v-else>
+				<div class="small-12 columns">
+					<v-btn id="attack" @click="attack">ATTACK</v-btn>
+					<v-btn id="special-attack" @click="specialAttack">SPECIAL ATTACK</v-btn>
+					<v-btn id="heal" @click="heal">HEAL</v-btn>
+					<v-btn id="give-up" @click="giveUp">GIVE UP</v-btn>
+				</div>
+			</section>
+			<section class="row log" v-if="turns.length > 0">
+				<div class="small-12 columns">
+					<ul>
+					<li v-for="turn in turns"
+						:class="{'player-turn': turn.isPlayer, 'monster-turn': !turn.isPlayer}" v-bind:key="turn.id">
+						{{ turn.text }}
+					</li>
+					</ul>
+				</div>
+			</section>
+		</v-container>
+	</div>
 </template>
 
 <script>
